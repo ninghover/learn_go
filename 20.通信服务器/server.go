af6782904d45728384bb5c944737d66586a62254
huang <huangh@stu.scu.edu.cn>
2024-12-09 17:20:31 +0800

    超时强踢

--- 20.通信服务器/server.go
+++ 20.通信服务器/server.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"runtime"
 	"sync"
+	"time"
 )
 
 type Server struct {
@@ -52,6 +54,9 @@ func (s *Server) Handle(conn net.Conn) {
 	user := NewUser(conn, s)
 	user.Online()
 
+	// 监听用户是否活跃的channel
+	isLive := make(chan bool)
+
 	//接收客户端发送的消息
 	go func() {
 		buf := make([]byte, 4096)
@@ -68,8 +73,28 @@ func (s *Server) Handle(conn net.Conn) {
 			msg := string(buf[:n-1]) //去除最后的\n
 
 			user.DoMessage(msg)
+
+			// 用户活跃
+			isLive <- true
 		}
 	}()
+
+	// 当前Handle阻塞
+	for {
+		select {
+		case <-isLive:
+			// 当前用户活跃，应该重置定时器
+			// 不做任何事情，为了激活select，更新定时器
+		case <-time.After(time.Second * 10): // 一旦执行这个case，会重置定时器
+			user.SendMsg("强制下线！\n")
+			// user.Offline() 为什么这里不需要Offline? 答:上面的for循环检测到n==0，会自动调用Offline
+
+			// 销毁资源
+			close(user.C)
+			conn.Close()
+			runtime.Goexit()
+		}
+	}
 
 }
 
