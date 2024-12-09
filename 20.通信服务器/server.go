ae3f168484955f1ec215e1d5fdf9c1191bf7bc6c
huang <huangh@stu.scu.edu.cn>
2024-12-09 14:55:43 +0800

    用户上线及广播功能

--- 20.通信服务器/server.go
+++ 20.通信服务器/server.go
@@ -3,25 +3,59 @@ package main
 import (
 	"fmt"
 	"net"
+	"sync"
 )
 
 type Server struct {
 	Ip   string
 	Port int
+	// 在线用户列表
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	//消息广播的channel
+	Message chan string
 }
 
 // 创建一个Server
-
 func NewServer(ip string, port int) *Server {
 	server := Server{
-		Ip:   ip,
-		Port: port,
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
 	}
 	return &server
 }
 
+// 监听Message广播消息channel，一旦有消息就发送给所有的User (单独启一个goroutine)
+func (s *Server) ListenMessage() {
+	for {
+		msg := <-s.Message
+		s.mapLock.Lock()
+		for _, cli := range s.OnlineMap {
+			cli.C <- msg
+		}
+		s.mapLock.Unlock()
+	}
+}
+
+// 广播消息
+func (s *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + msg
+	s.Message <- sendMsg
+}
+
 func (s *Server) Handle(conn net.Conn) {
-	fmt.Println("连接建立成功...")
+	// 用户上线，将用户加入到在线用户表中
+	user := NewUser(conn)
+	s.mapLock.Lock()
+	s.OnlineMap[user.Name] = user
+	s.mapLock.Unlock()
+
+	// 广播用户上线消息
+	s.BroadCast(user, "已上线")
+
 }
 
 // 启动服务器的接口
@@ -35,6 +69,9 @@ func (s *Server) Start() {
 
 	//close
 	defer Listener.Close()
+
+	// 启动监听Message的goroutine
+	go s.ListenMessage()
 
 	for {
 		//accept
